94b36b11c8d684aae9e72318e425471ede4621c1
Ian Baker <ianbaker615@gmail.com>
2024-03-06 10:21:16 -0800

    WIP: Implement Auth0-protected endpoints

--- auth0/src/main/java/dev/katyella/auth0/configs/SecurityConfig.java
+++ auth0/src/main/java/dev/katyella/auth0/configs/SecurityConfig.java
@@ -1,60 +1,60 @@
 package dev.katyella.auth0.configs;
 
-import dev.katyella.auth0.filters.JwtAuthenticationFilter;
-import dev.katyella.auth0.services.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        /*
+//        This is where we configure the security required for our endpoints and setup our app to serve as
+//        an OAuth2 Resource Server, using JWT validation.
+//        */
+//        return http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers(HttpMethod.POST, "/api/v1/signup", "/api/v1/signin").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/v1/test/**").permitAll()
+//                        .requestMatchers("/api/v1/???").hasAuthority("SCOPE_read:messages") // apply role-based permissions here?
+//                        .anyRequest().authenticated()
+//                )
+//                .cors(withDefaults())
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(withDefaults())
+//                )
+//                .build();
+//    }
+//}
+
+/**
+ * TEMPORARY Configures our application with Spring Security to restrict access to our API endpoints.
+ */
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
-
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService.userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/v1/signup", "/api/v1/signin").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/test/**").permitAll()
-                        .anyRequest().authenticated()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        This is where we configure the security required for our endpoints and setup our app to serve as
+        an OAuth2 Resource Server, using JWT validation.
+        */
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/public").permitAll()
+                        .requestMatchers("/api/private").authenticated()
+                        .requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
                 )
-                .authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .build();
     }
 }
---
+++ auth0/src/main/java/dev/katyella/auth0/models/Message.java
@@ -0,0 +1,3 @@
+package dev.katyella.auth0.models;
+
+public record Message(String message) {}
